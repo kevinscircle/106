8c6359dd1ae7771a10e044a19a1ab8645f08df61
kevinscircle@aol.com <kevingeoreyes@gmail.com>
2024-08-24 09:06:00 -0700

    bootstrap form

---
+++ scripts/task.js
@@ -0,0 +1,10 @@
+class Task {
+    constructor (d, desc, color, date, status, budget){
+        this.title = title;
+        this.desc = desc;
+        this.color = color;
+        this.date = date;
+        this.status = status;
+        this.budget = budget;
+    }
+}
